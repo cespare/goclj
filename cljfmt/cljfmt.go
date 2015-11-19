fba2ce04cc07cb94912c5d2b62bbca9b07c487d0
Caleb Spare <cespare@gmail.com>
2015-11-18 18:07:48 -0800

    [cljfmt] Handle dirs, symlinks, input from stdin
    Fixes #29.
    Fixes #30.

--- cljfmt/cljfmt.go
+++ cljfmt/cljfmt.go
@@ -1,89 +1,128 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 
-	"github.com/cespare/diff"
 	"github.com/cespare/goclj/format"
 	"github.com/cespare/goclj/parse"
 )
 
+func usage() {
+	fmt.Fprintf(os.Stderr, `usage: %s [flags] [paths...]
+Any directories given will be recursively walked. If no paths are provided,
+cljfmt reads from standard input.
+
+Flags:`, os.Args[0])
+	flag.PrintDefaults()
+}
+
 func main() {
 	var (
-		listDifferent = flag.Bool("l", false, "print files whose formatting differs from cljfmt's")
-		writeFile     = flag.Bool("w", false, "write result to (source) file instead of stdout")
+		list  = flag.Bool("l", false, "print files whose formatting differs from cljfmt's")
+		write = flag.Bool("w", false, "write result to (source) file instead of stdout")
 	)
+	flag.Usage = usage
 	flag.Parse()
-	if flag.NArg() < 1 {
-		usage()
-	}
-	if *listDifferent || *writeFile {
-		for _, filename := range flag.Args() {
-			if err := writeFormatted(filename, *listDifferent, *writeFile); err != nil {
-				fatal(err)
-			}
+
+	if flag.NArg() == 0 {
+		if *write {
+			fatal("cannot use -w with standard input")
+		}
+		if err := processFile("<stdin>", os.Stdin, false, false); err != nil {
+			fatal(err)
 		}
 		return
 	}
 
-	if flag.NArg() > 1 {
-		fatalf("must provide a single file unless -l or -w are given")
-	}
-
-	t, err := parse.File(flag.Arg(0), true)
-	if err != nil {
-		fatal(err)
-	}
-	p := format.NewPrinter(os.Stdout)
-	p.IndentChar = ' '
-	if err := p.PrintTree(t); err != nil {
-		fatal(err)
+	for _, path := range flag.Args() {
+		stat, err := os.Stat(path)
+		if err != nil {
+			fatal(err)
+		}
+		if stat.IsDir() {
+			walkDir(path, *list, *write)
+			continue
+		}
+		if err := processFile(path, nil, *list, *write); err != nil {
+			fatal(err)
+		}
 	}
 }
 
-func writeFormatted(filename string, listDifferent, writeFile bool) error {
-	tw, err := ioutil.TempFile(filepath.Dir(filename), "cljfmt-")
-	if err != nil {
-		return err
-	}
-	defer os.Remove(tw.Name())
-	defer tw.Close()
+var (
+	buf1 bytes.Buffer
+	buf2 bytes.Buffer
+)
 
-	t, err := parse.File(filename, true)
+// processFile formats the given file.
+// If in == nil, the input is the file of the given name.
+func processFile(filename string, in io.Reader, list, write bool) error {
+	if in == nil {
+		f, err := os.Open(filename)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		in = f
+	}
+
+	buf1.Reset()
+	buf2.Reset()
+
+	if _, err := io.Copy(&buf1, in); err != nil {
+		return err
+	}
+	t, err := parse.Reader(bytes.NewReader(buf1.Bytes()), filename, true)
 	if err != nil {
 		return err
 	}
-	p := format.NewPrinter(tw)
+
+	p := format.NewPrinter(&buf2)
 	p.IndentChar = ' '
 	if err := p.PrintTree(t); err != nil {
 		return err
 	}
-	tw.Close()
-	different, err := diff.Files(filename, tw.Name())
-	if err != nil {
-		return err
-	}
-	if !different {
+	if bytes.Equal(buf1.Bytes(), buf2.Bytes()) {
 		return nil
 	}
-	if listDifferent {
+	if list {
 		fmt.Println(filename)
 	}
-	if writeFile {
-		if err := os.Rename(tw.Name(), filename); err != nil {
+	if write {
+		if err := ioutil.WriteFile(filename, buf2.Bytes(), 0644); err != nil {
 			return err
 		}
+	}
+	if !list && !write {
+		io.Copy(os.Stdout, &buf2)
 	}
 	return nil
 }
 
-func usage() {
-	fmt.Fprintf(os.Stderr, "usage: %s [flags] [path ...]\n", os.Args[0])
-	os.Exit(1)
+func walkDir(path string, list, write bool) {
+	walk := func(path string, f os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		fmt.Println(path, f.IsDir())
+		if f.IsDir() {
+			return nil
+		}
+		if name := f.Name(); strings.HasPrefix(name, ".") || !strings.HasSuffix(name, ".clj") {
+			return nil
+		}
+		return processFile(path, nil, list, write)
+	}
+	if err := filepath.Walk(path, walk); err != nil {
+		fatal(err)
+	}
 }
 
 func fatal(args ...interface{}) {
